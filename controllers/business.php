6588f4840a91ecb3eb8f5aa65e72ff9eda285fc8
St-takashi <takashi@strategic.jp>
2022-01-12 17:59:54 +0900

    bay len nao

--- controllers/business.php
+++ controllers/business.php
@@ -113,6 +113,82 @@ class business extends controller{
 				);
 				$database->insert($data);
 				header("Location: /fukushisystem/business/A");
+			}else if(isset($_POST["btn_businessUpdate"])) {
+				// echo "<script>alert('受給者番号：');</script>";
+				// exit();
+					if (isset($arr[2])) {
+						$data = array(
+							'Model_number' 					=> $_POST["Model_number"],
+							'Model_symbol' 					=> $_POST["Model_symbol"],
+							'Service_offer_date' 			=> $_POST["Service_offer_date"],
+							'Corporate_name' 				=> $_POST["Corporate_name"],
+							'Representative' 				=> $_POST["Representative"],
+							'Businesser_name' 				=> $_POST["Businesser_name"],
+							'Abbreviation' 					=> $_POST["Abbreviation"],
+							'Administrator' 				=> $_POST["Administrator"],
+							'Businesser_number' 			=> $_POST["Businesser_number"],
+							'Businesser_postal_code' 		=> $_POST["Businesser_postal_code"],
+							'Businesser_address' 			=> $_POST["Businesser_address"],
+							'Businesser_phone' 				=> $_POST["Businesser_phone"],
+							'Kind' 							=> $_POST["Kind"],
+							'Evaluation_point_type' 		=> $_POST["Evaluation_point_type"],
+							'Public' 						=> $_POST["Public"],
+							'Area_type' 					=> $_POST["Area_type"],
+							'Capacity' 						=> $_POST["Capacity"],
+							'Capacity_type' 				=> $_POST["Capacity_type"],
+							'Work_migration_support' 		=> $_POST["Work_migration_support"],
+							'Work_settle_number' 			=> $_POST["Work_settle_number"],
+							'Vison_hear_support' 			=> $_POST["Vison_hear_support"],
+							'Welfare_pro_staff_add' 		=> $_POST["Welfare_pro_staff_add"],
+							'Severe_support_add1' 			=> $_POST["Severe_support_add1"],
+							'Severe_support_add2' 			=> $_POST["Severe_support_add2"],
+							'Care_link_nurse_staff' 		=> $_POST["Care_link_nurse_staff"],
+							'Exemption_measures' 			=> $_POST["Exemption_measures"],
+							'Pick_drop_kind_add' 			=> $_POST["Pick_drop_kind_add"],
+							'Self_result_unpulic' 			=> $_POST["Self_result_unpulic"],
+							'Body_not_abolition' 			=> $_POST["Body_not_abolition"],
+							'Wage_improve' 					=> $_POST["Wage_improve"],
+							'Area_life_support' 			=> $_POST["Area_life_support"],
+							'Overcapacity' 					=> $_POST["Overcapacity"],
+							'Employee_vacancy' 				=> $_POST["Employee_vacancy"],
+							'Service_admin_vacancy' 		=> $_POST["Service_admin_vacancy"],
+							'Treatment_improve' 			=> $_POST["Treatment_improve"],
+							'Treatment_improve_num' 		=> $_POST["Treatment_improve_num"],
+							'Treatment_career_improve' 		=> $_POST["Treatment_career_improve"],
+							'Special_treatment_improve_add' => $_POST["Special_treatment_improve_add"],
+							'Treatment_improve_add' 		=> $_POST["Treatment_improve_add"],
+							'Invoice_title' 				=> $_POST["Invoice_title"],
+							'Invoice_name' 					=> $_POST["Invoice_name"],
+							'User_invoice_remark' 			=> $_POST["User_invoice_remark"],
+							'Actual_cost1' 					=> $_POST["Actual_cost1"],
+							'Actual_cost2' 					=> $_POST["Actual_cost2"],
+							'Business_type' 				=> 1
+						);
+					// echo $arr[2];
+					// $data = array(['Activity_record_id', $arr[2] ]);
+					// foreach($data as $value){
+					// 	echo $value[0]."=".$value[1].$value[2];
+
+					// }
+					$list =  $database->update($data, array(['Business_id', $arr[2] ]));
+					// print_r($list);
+					// exit();
+					// header("Location: /fukushisystem/activeRecord/A/".$arr[2]."");
+					echo "<script>
+							UpdateActiveRecord();
+							function UpdateActiveRecord(page){
+						        var result = confirm('修正されました。');
+						        if (result) {
+						        	url = '/fukushisystem/business/A/".$arr[2]."';
+						        	window.location.href = url;
+						        }
+						    }
+
+						 </script>";
+
+					}else{
+						echo "<script>alert('受給者番号：');";
+					}
 			}
 		}else{
 			// echo "<script>alert('unit');</script>";
