c1d8867b31e7fd219575fc5ef78670d5cfb86681
St-takashi <takashi@strategic.jp>
2022-01-19 19:55:52 +0900

    msql 2022-01-19

--- controllers/memberMaster.php
+++ controllers/memberMaster.php
@@ -23,13 +23,14 @@ class memberMaster extends controller{
 			$paramsCurrentPage  = (!empty($_GET['page'])) ?  $_GET['page'] : 1;
 
 			// Query Count
-			$queryCount =  "SELECT COUNT(*) AS `total` FROM `unit` WHERE `Unit_id` > 0";
-
+			$queryCount =  "SELECT COUNT(*) AS `total` FROM `membermaster` WHERE `MemberMaster_Id` > 0";
+		
 			$totalItem = $database->fetchRow($queryCount)['total'];
+
 			$configPagination = ['totalItemsPerPage'	=> 10, 'pageRange' => 3, 'currentPage' => $paramsCurrentPage];
 			$objPagination 	 = new Pagination($totalItem, $configPagination);
 
-			$queryList[] = "SELECT * FROM unit ORDER BY Unit_id ASC";
+			$queryList[] = "SELECT * FROM membermaster ORDER BY MemberMaster_Id ASC";
 
 			// Pagination
 			if($objPagination->getTotalPage() > 1)  {
@@ -55,26 +56,26 @@ class memberMaster extends controller{
 		require_once("./views/head.html");
 		$db = $this->model("database");
 		$database = new Database();
-		$database->setTable("unit");
+		$database->setTable("membermaster");
 		if (isset($_SESSION['usernameSS'])&&$_SESSION['role']==0){
 			$arr = $this->UrlProcess();
 			$countItem;
 				// echo $arr[2];
 				if (isset($arr[2])) {
 					// Query Count
-					$queryCount =  "SELECT COUNT(*) AS `total` FROM `unit` WHERE `Unit_id` = ".$arr[2];
+					$queryCount =  "SELECT COUNT(*) AS `total` FROM `membermaster` WHERE `MemberMaster_Id` = ".$arr[2];
 					$countItem = $database->fetchRow($queryCount)['total'];
 				}
 
 				if ($countItem != 0) {
-					$queryList = "SELECT * FROM unit WHERE `Unit_id` = ".$arr[2];
+					$queryList = "SELECT * FROM membermaster WHERE `MemberMaster_Id` = ".$arr[2];
 					$listItem = $database->fetchRow($queryList);
 				}
 			require_once("./views/memberMasterNew.html");
 			// echo "<script>alert('ok');</script>";
 			
-			if (isset($_POST["btn_unit"])) {
-				$queryList = "SELECT  COUNT(`Unit_code`) AS `total` FROM unit WHERE Unit_code ="."'".$_POST["Unit_code"]."'";
+			if (isset($_POST["btn_memberMaster"])) {
+				$queryList = "SELECT  COUNT(`MemberMaster_Id`) AS `total` FROM membermaster WHERE MemberMaster_Id ="."'".$_POST["MemberMaster_Id"]."'";
 				// $queryList = implode(" ", $queryList);
 				$listItem = $database->fetchRow($queryList)['total'];
 				// print_r($listItem);
@@ -82,27 +83,33 @@ class memberMaster extends controller{
 				// exit();
 				if ($listItem==0) {
 					$data = array(
-						'Unit_code' 				=> $_POST["Unit_code"],
-						'Service_content' 			=> $_POST["Service_content"],
-						'Number_unit' 				=> $_POST["Number_unit"],
-						'Unit_price' 				=> $_POST["Unit_price"]
+						'CompanyName' 				=> $_POST["CompanyName"],
+						'PostCode' 					=> $_POST["PostCode"],
+						'Address' 					=> $_POST["Address"],
+						'TelephoneNumber' 			=> $_POST["TelephoneNumber"],
+						'PersonChargeName' 			=> $_POST["PersonChargeName"],
+						'PersonChargeInformation' 	=> $_POST["PersonChargeInformation"],
+						'EmailAddress' 				=> $_POST["EmailAddress"]
 					);
 				$database->insert($data);
-				header("Location: /fukushisystem/unit");
+				header("Location: /fukushisystem/memberMaster");
 				}else{
 					echo "<script>alert('サービスコードはありました。');</script>";
 				}
 			}else if(isset($_POST["btn_unitUpdate"])) {
 				// echo "<script>alert('受給者番号：');</script>";
-					$queryList = "SELECT  COUNT(`Unit_id`) AS `total` FROM unit WHERE `Unit_id` =".$arr[2];
+					$queryList = "SELECT  COUNT(`MemberMaster_Id`) AS `total` FROM membermaster WHERE `MemberMaster_Id` =".$arr[2];
 					// $queryList = implode(" ", $queryList);
 					$listItem = $database->fetchRow($queryList)['total'];
 					if ($listItem != 0) {
 						$data = array(
-							'Unit_code' 				=> $_POST["Unit_code"],
-							'Service_content' 			=> $_POST["Service_content"],
-							'Number_unit' 				=> $_POST["Number_unit"],
-							'Unit_price' 				=> $_POST["Unit_price"]
+							'CompanyName' 				=> $_POST["CompanyName"],
+							'PostCode' 					=> $_POST["PostCode"],
+							'Address' 					=> $_POST["Address"],
+							'TelephoneNumber' 			=> $_POST["TelephoneNumber"],
+							'PersonChargeName' 			=> $_POST["PersonChargeName"],
+							'PersonChargeInformation' 	=> $_POST["PersonChargeInformation"],
+							'EmailAddress' 				=> $_POST["EmailAddress"]
 						);
 					// echo $arr[2];
 					// $data = array(['Activity_record_id', $arr[2] ]);
@@ -110,16 +117,16 @@ class memberMaster extends controller{
 					// 	echo $value[0]."=".$value[1].$value[2];
 
 					// }
-					$list =  $database->update($data, array(['Unit_id', $arr[2] ]));
+					$list =  $database->update($data, array(['MemberMaster_Id', $arr[2] ]));
 					// print_r($list);
 					// exit();
 					// header("Location: /fukushisystem/activeRecord/A/".$arr[2]."");
 					echo "<script>
-							UpdateUnit();
-							function UpdateUnit(){
+							UpdateMemberMaster();
+							function UpdateMemberMaster(){
 						        var result = confirm('修正されました。');
 						        if (result) {
-						        	url = '/fukushisystem/unit/Update/".$arr[2]."';
+						        	url = '/fukushisystem/memberMaster/Update/".$arr[2]."';
 						        	window.location.href = url;
 						        }
 						    }
